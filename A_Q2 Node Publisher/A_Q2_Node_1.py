6b2d5dfdb7cbb7977d3dd65b3012768da4094f1c
Jnaneshrompilli <79641410+Jnaneshrompilli@users.noreply.github.com>
2021-04-25 09:19:59 +0530

    Update A_Q2_Node_1.py

--- A_Q2 Node Publisher/A_Q2_Node_1.py
+++ A_Q2 Node Publisher/A_Q2_Node_1.py
@@ -23,16 +23,20 @@ from std_msgs.msg import String
 
 # Function publishing topic
 def talker():
+    # Assigning publisher
     pub = rospy.Publisher('team_abhiyaan',String,queue_size=10)
+    # Registering this node In ROS with unique identity 
     rospy.init_node('talker_n1',anonymous=True)
-    # Rate at which function publishes 
+    # Rate at which function publishes (1 msg/sec) 
     rate = rospy.Rate(1)
 
     # Runs utill we shut down the Node
     while not rospy.is_shutdown():
+        # Message to be published
         msg_str = "Team Abhiyaan"
         rospy.loginfo(msg_str)
         pub.publish(msg_str)
+        # Rate at which loop will run
         rate.sleep()
 
 
