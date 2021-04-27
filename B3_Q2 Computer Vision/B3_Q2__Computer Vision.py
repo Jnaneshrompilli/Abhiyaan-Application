bcd4218636c14e13dad845c278263655c343bac1
Jnaneshrompilli <79641410+Jnaneshrompilli@users.noreply.github.com>
2021-04-27 21:04:35 +0530

    Update B3_Q2__Computer Vision.py

--- B3_Q2 Computer Vision/B3_Q2__Computer Vision.py
+++ B3_Q2 Computer Vision/B3_Q2__Computer Vision.py
@@ -1,4 +1,4 @@
-'''
+"""
 *************************************************************************************************************
                             SECTION-B
 SUBSECTION B.3
@@ -11,22 +11,26 @@ Q) OpenCV is the most used image processing library on the planet! So it
 Ans explanation:
   Basic idea behind detecting movement is to find the differences between two consecutive frames and locating contours
 
-*************************************************************************************************************'''
+*************************************************************************************************************"""
 
 
-import cv2 as  cv
+import cv2 as cv
 import numpy as np
 
 # Capturing video
-cap = cv.VideoCapture('videos\pedestrians.mp4')  #Input your Video
-frame_width = int( cap.get(cv.CAP_PROP_FRAME_WIDTH))
-frame_height =int( cap.get( cv.CAP_PROP_FRAME_HEIGHT))
+# Enter a valid Image Path
+# Try to give Input as RAW string
+cap = cv.VideoCapture(
+    r"C:\Users\Admin\Desktop\ML_Projects\Image rec\videos\pedestrians.mp4"
+)  # Input your Video
+frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # For exporting frames into video
-fourcc = cv.VideoWriter_fourcc('X','V','I','D')
-out = cv.VideoWriter("test_case.mp4", fourcc, 5.0, (1280,720))
+fourcc = cv.VideoWriter_fourcc("X", "V", "I", "D")
+out = cv.VideoWriter("test_case1.mp4", fourcc, 5.0, (1280, 720))
 
-# Reading first two frames 
+# Reading first two frames
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
@@ -34,10 +38,10 @@ ret, frame2 = cap.read()
 while cap.isOpened():
     # Finding the difference between two frames
     diff = cv.absdiff(frame1, frame2)
-    #Converting into grayscale
+    # Converting into grayscale
     gray = cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
-    #Blurring image to reduce noise
-    blur = cv.GaussianBlur(gray, (5,5), 0)
+    # Blurring image to reduce noise
+    blur = cv.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
     dilated = cv.dilate(thresh, None, iterations=3)
     # Finding the location of contours
@@ -49,9 +53,9 @@ while cap.isOpened():
         # Neglecting minor contours (if present)
         if cv.contourArea(contour) < 450:
             continue
-        cv.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    
-    image = cv.resize(frame1, (1280,720))
+        cv.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    image = cv.resize(frame1, (1280, 720))
     # Exporting frame to th output video
     out.write(image)
     cv.imshow("Tracking Output", frame1)
@@ -66,3 +70,4 @@ while cap.isOpened():
 cv.destroyAllWindows()
 cap.release()
 out.release()
+
